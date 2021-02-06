43f69550b0f394143262d1a02d840d69cea8dc62
Dan Tulovsky <dant@wetsnow.com>
2021-02-06 18:30:54 -0500

    updates!

---
+++ build_push.sh
@@ -0,0 +1,14 @@
+export IMAGE_NAME="frontend"
+export IMAGE_ID="ghcr.io/dantulovsky/web-static/$IMAGE_NAME"
+export VERSION="v2020020600"
+
+echo "Building local/$IMAGE_NAME"
+docker build . --file Dockerfile --tag local/$IMAGE_NAME
+
+echo "Tagging local/$IMAGE_NAME $IMAGE_ID:$VERSION"
+docker tag local/$IMAGE_NAME $IMAGE_ID:$VERSION
+docker tag local/$IMAGE_NAME $IMAGE_ID:latest
+
+echo "Pushing $IMAGE_ID:$VERSION"
+docker push $IMAGE_ID:$VERSION
+docker push $IMAGE_ID:$latest
