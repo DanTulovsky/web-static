f89eb29edd4c71e1161d44921dbc62760cd98fc3
Dan Tulovsky <dant@wetsnow.com>
2023-08-20 11:43:57 -0400

    web upgrade.

--- build_push.sh
+++ build_push.sh
@@ -1,6 +1,6 @@
 export IMAGE_NAME="frontend"
-export IMAGE_ID="ghcr.io/dantulovsky/web-static/$IMAGE_NAME"
-export VERSION="0.0.13"
+
+export VERSION="0.0.44"
 
 echo "Building local/$IMAGE_NAME"
 docker build . --file Dockerfile --tag local/$IMAGE_NAME
